450092ccdf049ba8cbcacfc32cdf654e34f435e8
Matheus Augusto Daminelli <matheus.daminelli@gmail.com>
2024-05-08 19:36:54 -0300

    adiconando a troca de telas e as telas faltantes

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def hello():
     return render_template("menu.html")
 
+@app.route("/cadastro")  
+def cadastro():
+    return render_template("cadastro.html")  
+
+@app.route("/ajuste")  
+def ajuste():
+    return render_template("ajuste.html")  
+
 if __name__ == "__main__":
     app.run(debug=True)
